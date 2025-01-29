d73d401f93c6327dfb9cb87591ce7d79722dd1dc
Christopher Eagle <chris.eagle@get10up.com>
2025-01-29 20:52:57 +0000

    formatting

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,22 +1,22 @@
-import { sanityFetch } from '@/sanity/lib/live'
-import { allPostsQuery } from '@/sanity/lib/queries'
-import { Post as PostType } from '@/sanity.types'
-import Post from '@/components/Post'
+import { sanityFetch } from '@/sanity/lib/live';
+import { allPostsQuery } from '@/sanity/queries/queries';
+import { Post as PostType } from '@/sanity.types';
+import Post from '@/components/Post';
 
 export default async function Posts() {
-  const { data: posts } = await sanityFetch({ query: allPostsQuery })
+  const { data: posts } = await sanityFetch({ query: allPostsQuery });
 
   if (!posts) {
-    return null
+    return null;
   }
 
   return (
     <div>
       {posts.map((post) => {
         if (post) {
-          return <Post key={post._id} post={post as unknown as PostType} />
+          return <Post key={post._id} post={post as unknown as PostType} />;
         }
       })}
     </div>
-  )
+  );
 }
