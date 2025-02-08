89ba7d7108684ab980e0c233b7c854cd99fa1283
lcnogueira <luizcns18@gmail.com>
2025-02-08 12:24:53 -0300

    delete demo cached segment

--- src/sanity/lib/cached.ts
+++ src/sanity/lib/cached.ts
@@ -5,7 +5,7 @@ import { client } from './client';
  * Use this helper fetch function if you want to cache the requests and then revalidate them based on the tags.
  * You can use a webhook to trigger the revalidation or you can use the live client to listen for events and then revalidate the content.
  * You can find an example of how to cache and revalidate the content in the
- * [next-sanity package: https://github.com/sanity-io/next-sanity/tree/main/packages/next-sanity#sanityfetch-helper-function).
+ * [next-sanity package] https://github.com/sanity-io/next-sanity/tree/main/packages/next-sanity#sanityfetch-helper-function).
  *
  * If you plan to have the content being live updated on the frontend, then use the sanityLiveFetch function instead.
  */
