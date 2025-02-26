6e6ed57f0fbc00160f3624550ee5d7238685dd99
Christopher Eagle <chris.eagle@get10up.com>
2025-02-26 20:48:56 +0000

    author component

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -1,9 +1,21 @@
-import { Person } from '@/sanity.types'
+import { Person } from '@/sanity.types';
+import * as v from 'valibot';
+
+const propSchema = v.object({
+  firstName: v.string(),
+  lastName: v.string(),
+});
 
 export default function Author({ author }: { author: Person }) {
+  const result = v.safeParse(propSchema, author);
+
+  if (!result.success) {
+    return null;
+  }
+
   return (
     <div>
       {author.firstName} {author.lastName}
     </div>
-  )
+  );
 }
