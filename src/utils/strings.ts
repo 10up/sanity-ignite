9b90a707670bc125a586dcf457521fde7909b4b5
lcnogueira <luizcns18@gmail.com>
2025-03-11 22:59:15 -0300

    move functions to same strings util file

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,3 +1,22 @@
+import type { PortableTextBlock } from 'next-sanity';
+
+export const slugifyRegex = /^[a-zA-Z0-9-]+$/u;
+
+export const slugify = (value: string) =>
+  value
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .slice(0, 200)
+    .split('')
+    .filter((char) => slugifyRegex.test(char))
+    .join('');
+
+export function parseChildrenToSlug(children: PortableTextBlock['children']) {
+  if (!children) return '';
+  return slugify(children.map((child) => child.text).join(''));
+}
+
 export const capitalize = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
