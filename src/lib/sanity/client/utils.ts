1566555be9faa31b461a38745e986cdfa94bbbf6
lcnogueira <luizcns18@gmail.com>
2025-03-15 14:24:33 -0300

    solve type issues

--- src/lib/sanity/client/utils.ts
+++ src/lib/sanity/client/utils.ts
@@ -1,15 +1,18 @@
 import createImageUrlBuilder from '@sanity/image-url';
 import { clientEnv } from '@/env/clientEnv';
 import { createDataAttribute, type CreateDataAttributeProps } from 'next-sanity';
-import type { SanityAsset } from '@sanity/image-url/lib/types/types';
-
+import { CustomImageType } from '@/types/seo';
+import { GetPageQueryResult } from '@/sanity.types';
 const imageBuilder = createImageUrlBuilder({
   projectId: clientEnv.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: clientEnv.NEXT_PUBLIC_SANITY_DATASET,
 });
 
-export const urlForImage = (source: SanityAsset) => {
-  // Ensure that source image contains a valid reference
+export type ImageSource = NonNullable<
+  NonNullable<GetPageQueryResult>['pageSections']
+>[number]['image'];
+
+export const urlForImage = (source: ImageSource) => {
   if (!source?.asset?._ref) {
     return undefined;
   }
@@ -17,7 +20,11 @@ export const urlForImage = (source: SanityAsset) => {
   return imageBuilder?.image(source).auto('format').fit('max');
 };
 
-export function resolveOpenGraphImage(image: SanityAsset | undefined, width = 1200, height = 627) {
+export function resolveOpenGraphImage(
+  image: CustomImageType | undefined,
+  width = 1200,
+  height = 627,
+) {
   if (!image) return;
   const url = imageBuilder.image(image)?.width(width).height(height).fit('crop').url();
   if (!url) return;
