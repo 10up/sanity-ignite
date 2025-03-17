644e45a20c9ad4d0b823b1930828629f1c8ee50c
Taylor Lovett <tlovett88@gmail.com>
2025-03-17 14:12:40 -0400

    Link author

--- src/components/modules/Author.tsx
+++
@@ -1,9 +0,0 @@
-import { Person } from '@/sanity.types';
-
-export default function Author({ author }: { author: Pick<Person, 'firstName' | 'lastName'> }) {
-  return (
-    <div>
-      {author.firstName} {author.lastName}
-    </div>
-  );
-}
